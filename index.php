4053db1eea8e935daca3847c2dcc287094e1e877
stepheno42 <stephenmakuo8>
2023-01-29 14:39:54 +0100

    adding new functions

--- index.php
+++ index.php
@@ -36,31 +36,31 @@
         <div class="row" data-aos="fade-up">
           <div class="col-lg-12 d-flex justify-content-center">
             <ul id="portfolio-flters">
-              <!-- <li data-filter="*" >All</li> -->
-              <!-- <li data-filter=".filter-app">App</li> -->
+              <li data-filter="*"class="filter-active" >All</li>
+              <li data-filter=".filter-offline">Offline webs</li>
               <!-- <li data-filter=".filter-card">Card</li> -->
-              <li data-filter=".filter-web" class="filter-active">Web</li>
+              <li data-filter=".filter-web">Online Webs</li>
             </ul>
           </div>
         </div>
 
         <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="100">
 
-          <!-- <div class="col-lg-4 col-md-6 portfolio-item filter-app" >
+          <div class="col-lg-4 col-md-6 portfolio-item filter-app" >
             <div class="portfolio-wrap">
-              <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
+              <img src="assets/img/portfolio/laragigs.png" class="img-fluid" alt="">
               <div class="portfolio-links">
-                <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
-                <a href="portfolio-details.php" title="More Details"><i class="bx bx-link"></i></a>
+                <a href="assets/img/portfolio/laragigs.png" data-gallery="portfolioGallery" class="portfolio-lightbox" title="App 1"><i class="bx bx-plus"></i></a>
+                <a href="laragigs-details.php" title="More Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
-          </div> -->
+          </div>
 
-          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+          <div class="col-lg-4 col-md-6 portfolio-item filter-offline">
             <div class="portfolio-wrap">
               <img src="assets/img/portfolio/portfolio-6.jpg" class="img-fluid" alt="">
               <div class="portfolio-links">
-                <a href="assets/img/portfolio/portfolio-5.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 2"><i class="bx bx-plus"></i></a>
+                <a href="assets/img/portfolio/portfolio-6.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 2"><i class="bx bx-plus"></i></a>
                 <a href="jumia-details.php" title="More Details"><i class="bx bx-link"></i></a>
               </div>
             </div>
---
+++ laragigs-details.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <?php include 'includes/head.php'; ?>
+
+
+<body>
+
+  <!-- ======= Mobile nav toggle button ======= -->
+  <i class="bi bi-list mobile-nav-toggle d-xl-none"></i>
+
+  <!-- ======= Header ======= -->
+
+    <?php include 'includes/header.php' ?>
+
+  <!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Portfoio Details</h2>
+          <ol>
+            <li><a href="index.php">Home</a></li>
+            <li>Portfoio Details</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <!-- ======= Portfolio Details Section ======= -->
+    <section id="portfolio-details" class="portfolio-details">
+      <div class="container">
+
+        <div class="row gy-4">
+
+          <div class="col-lg-8">
+            <div class="portfolio-details-slider swiper">
+              <div class="swiper-wrapper align-items-center">
+
+                <div class="swiper-slide">
+                  <img src="assets/img/portfolio/laragigs1.png" alt="">
+                </div>
+
+                <div class="swiper-slide">
+                  <img src="assets/img/portfolio/lara.png" alt="">
+                </div>
+
+                <div class="swiper-slide">
+                  <img src="assets/img/portfolio/laragigs.png" alt="">
+                </div>
+
+              </div>
+              <div class="swiper-pagination"></div>
+            </div>
+          </div>
+
+          <div class="col-lg-4">
+            <div class="portfolio-info">
+              <h3>Project information</h3>
+              <ul>
+                <li><strong>Category</strong>: Web design</li>
+                <li><strong>Client</strong>: Personal Project</li>
+                <li><strong>Project date</strong>: March, 2021</li>
+                <li><strong>Project URL</strong>: <a href="https://github.com/stephen042/main-jumia-clone">Github Repo</a></li>
+              </ul>
+            </div>
+            <div class="portfolio-description">
+              <h2>Laragigs details</h2>
+              <p>
+               Laragigs is a simple Laravel project. It is used to post jobs and gigs. 
+              </p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Portfolio Details Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <?php include 'includes/footer.php' ?>
+  <!-- End  Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <?php include 'includes/script.php' ?>
+
+</body>
+
+</html>
