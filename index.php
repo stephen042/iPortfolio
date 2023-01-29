0bf3d781831f4a8f0d88b03499f26d155f758b01
stepheno42 <stephenmakuo8>
2023-01-29 15:04:35 +0100

    pic

--- index.php
+++ index.php
@@ -154,9 +154,6 @@
         </div>
 
         <div class="row">
-          <div class="col-lg-4" data-aos="fade-right">
-            <img src="assets/img/profile-img.jpg" class="img-fluid" alt="">
-          </div>
           <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
             <h3> Web Developer.</h3>
             <p class="fst-italic">
